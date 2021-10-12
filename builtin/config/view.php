f7ad0f8819f1f817759e561da6f8991aa0414f62
Zain ul abidin <zain.abidin@paybreak.com.au>
2021-10-12 13:43:23 +1100

    separate db config and app config and kint in xdebug

--- builtin/config/view.php
+++ builtin/config/view.php
@@ -7,31 +7,46 @@
  */
 $magentoInc->setAdminHtml();
 
-Class ConfigView
+class ConfigView
 {
 
     /**
      * @var \Magento\Framework\App\ResourceConnection
      */
     private $resourceConnection;
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $config;
 
-    function __construct(\Magento\Framework\App\ResourceConnection $resourceConnection)
+    function __construct(
+        \Magento\Framework\App\ResourceConnection $resourceConnection,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config
+    )
     {
 
         $this->resourceConnection = $resourceConnection;
+        $this->config = $config;
     }
-    protected function getConnection() : \Magento\Framework\DB\Adapter\AdapterInterface
+
+    protected function getConnection(): \Magento\Framework\DB\Adapter\AdapterInterface
     {
         return $this->resourceConnection->getConnection();
     }
+
     protected function getAllRows(string $vSql)
     {
-        return  $aReturn = $this->getConnection()->fetchAssoc($vSql);
+        return $aReturn = $this->getConnection()->fetchAssoc($vSql);
     }
 
-    public function getConfig(string $path)
+    public function getDbConfig(string $path)
     {
         return $this->getAllRows("select * from core_config_data where path like '%$path%'");
+    }
+
+    public function getDefaultConfig(string $path)
+    {
+        return $this->config->getValue($path);
     }
 }
 
