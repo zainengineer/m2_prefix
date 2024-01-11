691036033ee9d0fe135a627a6e33ec5812d81420
Zain ul abidin <zain.ulabidin@munrofg.com>
2024-01-11 22:10:56 +1100

    display xdebug mode as well

--- snippets_core_pre/xdebug.php
+++ snippets_core_pre/xdebug.php
@@ -1,4 +1,10 @@
 <?php
+xdebug_break();
 $debugIt = 1;
-d(1);
+echo date('c') . "\n<br/>";
+$xdebugMode = ini_get('xdebug.mode');;
+d($xdebugMode);
+if ($xdebugMode!=='debug'){
+    d("xdebug needs debug mode");
+}
 if (function_exists('xdebug_break')){xdebug_break();die('break after xdebug');} else{die('xdebug does not exist');}
